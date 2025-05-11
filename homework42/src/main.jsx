72e6937eac9c2ca5982235c94fd3de8e2e65807f
DariaDrob <daradrobotenko@gmail.com>
2025-05-11 09:07:10 +0200

    Uploaded

--- homework42/src/main.jsx
+++ homework42/src/main.jsx
@@ -1,21 +1,10 @@
-import React, { useContext, useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeContext } from './context/ThemeContext';
 import App from './App';
 import './index.css';
 
-function Root() {
-    const { theme } = useContext(ThemeContext);
-
-    useEffect(() => {
-        document.body.setAttribute('data-theme', theme);
-    }, [theme]);
-
-    return <App />;
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Root />
+        <App />
     </React.StrictMode>
 );
